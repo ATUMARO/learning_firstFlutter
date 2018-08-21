c4f60a092e841a200299500863911690a8b3cd12
bingningO <zhubingning123@gmail.com>
2018-08-21 23:40:05 +0900

    クラス分け、シンプルリスト、画面遷移

---
+++ lib/NewSection.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:projects/ListActivity.dart';
+
+class NewSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+        color: Colors.blueGrey,
+        width: 300.0,
+        padding: const EdgeInsets.all(32.0),
+        child:
+        Center(
+            child: Column(
+              children: <Widget>[
+                createColumn(context, 1),
+                createColumn(context, 2),
+                createColumn(context, 3),
+                createColumn(context, 4),
+                createColumn(context, 5),
+              ],
+            )
+        )
+    );
+  }
+
+  Widget createColumn(BuildContext context, int index) {
+    return InkWell(
+        onTap: () {
+          print('click tapped!');
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ListActivity(index)),
+          );
+        },
+      child: Center(
+        child: Container(
+          color: Colors.blue[800],
+          margin: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8.0),
+          child: Text('jump to item $index'),
+        ),
+      ),
+    );
+  }
+
+}
